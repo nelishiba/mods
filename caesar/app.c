201e7699ea738901491b4a5e902ae519aed81528
Hiroki Watanabe <nelio@inl.ics.keio.ac.jp>
2018-07-02 08:52:04 +0000

    write can be called once

---
+++ caesar/app.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define DEVFILE "/dev/caesar"
+#define STRING1 "hello, world!"
+#define STRING2 "Don't be Lazy!"
+
+int open_file(char *filename)
+{
+	int fd;
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1) {
+		perror("open");
+	}
+	return fd;
+}
+
+void close_file(int fd)
+{
+	if (close(fd) < 0) {
+		perror("close");
+	}
+}
+
+void read_file(int fd, int len)
+{
+	unsigned char buf[256];
+
+	ssize_t ret;
+
+	ret = read(fd, buf, len);
+	if (ret > 0) {
+		printf("%s\n", buf);
+	} else {
+		perror("read");
+	}
+}
+
+void write_file(int fd, char *buf)
+{
+	ssize_t ret;
+
+	ret = write(fd, buf, strlen(buf));
+	if (ret <= 0) {
+		perror("write");
+	}
+}
+
+int main(void)
+{
+	int fd;
+	char *str1 = STRING1;
+	char *str2 = STRING2;
+
+	fd = open_file(DEVFILE);
+
+	write_file(fd, str1);
+	read_file(fd, strlen(str1));
+
+// write_file(fd, str2);
+	read_file(fd, strlen(str2));
+
+	close_file(fd);
+
+	return 0;
+}
