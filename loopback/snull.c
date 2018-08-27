b8d3149073044c11a6f09a9c384ec5c01c16f8dd
Hiroki Watanabe <nelio@inl.ics.keio.ac.jp>
2018-08-27 08:43:45 +0000

    tmp

--- loopback/snull.c
+++ loopback/snull.c
@@ -20,6 +20,8 @@ MODULE_AUTHOR("Hiroki Watanabe");
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define MODULE_NAME "SNULL"
+#define SNULL_RX_INTR 0x0001
+#define SNULL_TX_INTR 0x0002
 
 struct net_device *snull_devs[2];
 
@@ -49,6 +51,7 @@ struct snull_packet {
 /* private data structure to each device */
 struct snull_priv {
 	struct net_device_stats stats;
+	int status;
 	struct snull_packet *ppool;
 	struct snull_packet *rx_queue;  /* List of incoming packets */
 	int rx_int_enabled;
@@ -69,7 +72,63 @@ static u32 always_on(struct net_device *dev) {
 	return 1;
 }
 
-//static void (*snull_interrupt)(int, void *, struct pt_regs *);
+static void (*snull_interrupt)(int, void *, struct pt_regs *);
+
+
+void snull_enqueue_buf(struct net_device *dev, struct snull_packet *pkt)
+{
+	unsigned long flags;
+	struct snull_priv *priv = netdev_priv(dev);
+	
+	spin_lock_irqsave(&priv->lock, flags);
+	pkt->next = priv->rx_queue;
+	priv->rx_queue = pkt;
+	spin_unlock_irqrestore(&priv->lock, flags);
+}
+
+struct snull_packet *snull_dequeue_buf(struct net_device *dev)
+{
+	struct snull_priv *priv = netdev_priv(dev);
+	struct snull_packet *pkt;
+	unsigned long flags;
+
+	spin_lock_irqsave(&priv->lock, flags);
+	pkt = priv->rx_queue;
+	if (pkt != NULL)
+		priv->rx_queue = pkt->next;
+	spin_unlock_irqrestore(&priv->lock, flags);
+	return pkt;
+}
+
+struct snull_packet *snull_get_tx_buffer(struct net_device *dev)
+{
+	struct snull_priv *priv = netdev_priv(dev);
+	unsigned long flags;
+	struct snull_packet *pkt;
+
+	spin_lock_irqsave(&priv->lock, flags);
+	pkt = priv->ppool;
+	priv->ppool = pkt->next;
+	if (priv->ppool == NULL) {
+		printk(KERN_INFO MODULE_NAME ": Pool empty\n");
+		netif_stop_queue(dev);
+	}
+	spin_unlock_irqrestore(&priv->lock, flags);
+	return pkt;
+}
+
+void snull_release_buffer(struct snull_packet *pkt)
+{
+	unsigned long flags;
+	struct snull_priv *priv = netdev_priv(pkt->dev);
+	
+	spin_lock_irqsave(&priv->lock, flags);
+	pkt->next = priv->ppool;
+	priv->ppool = pkt;
+	spin_unlock_irqrestore(&priv->lock, flags);
+	if (netif_queue_stopped(pkt->dev) && pkt->next == NULL)
+		netif_wake_queue(pkt->dev);
+}
 
 /*
  * Enable and disable receive interrupts.
@@ -109,6 +168,44 @@ static void snull_hw_tx(char *buf, int len, struct net_device *dev)
 	ih->check = 0; /* and rebuild the checksum (ip needs it)  */
 	ih->check = ip_fast_csum((unsigned char *)ih, ih->ihl);
 	printk(KERN_INFO MODULE_NAME ": ih->check = %d\n", ih->check);
+
+	if (dev == snull_devs[0]) {
+		printk(KERN_INFO MODULE_NAME ": %08x:%5i --> %08x:%05i\n",
+				ntohl(ih->saddr), ntohs(((struct tcphdr *)(ih+1))->source),
+				ntohl(ih->daddr), ntohs(((struct tcphdr *)(ih+1))->dest)
+			   );
+	} else {
+		printk(KERN_INFO MODULE_NAME ": %08x:%5i --> %08x:%05i\n",
+				ntohl(ih->saddr), ntohs(((struct tcphdr *)(ih+1))->source),
+				ntohl(ih->daddr), ntohs(((struct tcphdr *)(ih+1))->dest)
+			   );
+	}
+
+	dest = snull_devs[dev == snull_devs[0] ? 1 : 0];
+	priv = netdev_priv(dest);
+	tx_buffer = snull_get_tx_buffer(dev);
+	tx_buffer->datalen = len;
+	memcpy(tx_buffer->data, buf, len);
+	snull_enqueue_buf(dest, tx_buffer);
+
+	if (priv->rx_int_enabled) {
+		priv->status |= SNULL_RX_INTR;
+		snull_interrupt(0, dest, NULL);
+	}
+
+	priv = netdev_priv(dev);
+	priv->tx_packetlen = len;
+	priv->tx_packetdata = buf;
+	priv->status |= SNULL_TX_INTR;
+
+	if (lockup && ((priv->stats.tx_packets + 1) % lockup) == 0) {
+		/* simulate a dropped transmit interrupt */
+		netif_stop_queue(dev);
+		printk(KERN_INFO MODULE_NAME ": Simulate lockup at %ld, txp %ld\n", 
+				jiffies, (unsigned long)priv->stats.tx_packets);
+	} else {
+		snull_interrupt(0, dev, NULL);
+	}
 
 }
 
@@ -189,6 +286,81 @@ int snull_tx(struct sk_buff *skb, struct net_device *dev)
 	return len;
 }
 
+void snull_rx(struct net_device *dev, struct snull_packet *pkt)
+{
+	struct sk_buff *skb;
+	struct snull_priv *priv = netdev_priv(dev);
+
+	/*
+	 * The packet has been retrieved from the transmission
+	 * medium. Build an skb around it, so upper layers can handle it
+	 */
+	skb = dev_alloc_skb(pkt->datalen + 2);
+	if (!skb) {
+		if (printk_ratelimit())
+			printk(KERN_NOTICE "snull rx: low on mem - packet dropped\n");
+		priv->stats.rx_dropped++;
+		goto out;
+	}
+	skb_reserve(skb, 2); /* align IP on 16B boundary */  
+	memcpy(skb_put(skb, pkt->datalen), pkt->data, pkt->datalen);
+
+	/* Write metadata, and then pass to the receive level */
+	skb->dev = dev;
+	skb->protocol = eth_type_trans(skb, dev);
+	skb->ip_summed = CHECKSUM_UNNECESSARY; /* don't check it */
+	priv->stats.rx_packets++;
+	priv->stats.rx_bytes += pkt->datalen;
+	netif_rx(skb);
+  out:
+	return;
+}
+
+static void snull_regular_interrupt(int irq, void *dev_id, struct pt_regs *regs)
+{
+	int statusword;
+	struct snull_priv *priv;
+	struct snull_packet *pkt = NULL;
+	/*
+	 * As usual, check the "device" pointer to be sure it is
+	 * really interrupting.
+	 * Then assign "struct device *dev"
+	 */
+	struct net_device *dev = (struct net_device *)dev_id;
+	/* ... and check with hw if it's really ours */
+
+	/* paranoid */
+	if (!dev)
+		return;
+
+	/* Lock the device */
+	priv = netdev_priv(dev);
+	spin_lock(&priv->lock);
+
+	/* retrieve statusword: real netdevices use I/O instructions */
+	statusword = priv->status;
+	priv->status = 0;
+	if (statusword & SNULL_RX_INTR) {
+		/* send it to snull_rx for handling */
+		pkt = priv->rx_queue;
+		if (pkt) {
+			priv->rx_queue = pkt->next;
+			snull_rx(dev, pkt);
+		}
+	}
+	if (statusword & SNULL_TX_INTR) {
+		/* a transmission is over: free the skb */
+		priv->stats.tx_packets++;
+		priv->stats.tx_bytes += priv->tx_packetlen;
+		dev_kfree_skb(priv->skb);
+	}
+
+	/* Unlock the device and we are done */
+	spin_unlock(&priv->lock);
+	if (pkt) snull_release_buffer(pkt); /* Do this outside the lock! */
+	return;
+}
+
 static const struct net_device_ops snull_ops = {
 	.ndo_open = snull_open,
 	.ndo_stop = snull_stop,
@@ -248,6 +420,7 @@ int snull_init(void)
 	int ret = -ENOMEM;
 	printk(KERN_INFO MODULE_NAME ": start loading...\n");
 
+	snull_interrupt = snull_regular_interrupt;
 
 	/* allocate the devices */
 	snull_devs[0] = alloc_netdev(sizeof(struct snull_priv), "sn%d", NET_NAME_UNKNOWN, snull_setup);
